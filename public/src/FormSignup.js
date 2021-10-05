a718dc48401681206c36d585649a1d87e8686d27
Vish-wassn <88095211+Vish-wassn@users.noreply.github.com>
2021-10-05 13:26:59 +0530

    Add files via upload

---
+++ public/src/FormSignup.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const FormSignup = () => {
+    return (
+        <div>
+            <h1>This is works fine</h1>
+        </div>
+    )
+}
+
+
+export default FormSignup
